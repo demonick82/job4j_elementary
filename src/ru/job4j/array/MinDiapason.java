cb6c532247cded17708d466712e73321fa7df257
demonick82 <demonick82@gmail.com>
2021-02-19 22:12:02 +0300

    6.5.3. Сортировка выборкой. [#469542]

--- src/ru/job4j/array/MinDiapason.java
+++ src/ru/job4j/array/MinDiapason.java
@@ -3,13 +3,12 @@ package ru.job4j.array;
 public class MinDiapason {
     public static int findMin(int[] array, int start, int finish) {
         int min = array[start];
-        if (finish < array.length - 1) {
-            for (int index = start; index <= finish; index++) {
-                if (array[index] < min) {
-                    min = array[index];
-                }
+        for (int index = start; index <= finish; index++) {
+            if (array[index] < min) {
+                min = array[index];
             }
         }
+
         return min;
     }
 }
